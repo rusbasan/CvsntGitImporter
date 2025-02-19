a71e9e055d454dc42e600aeac70ffe303cd2cd6e
Bob Rusbasan <bob@rusbasan.com>
2025-02-19 11:50:05 -0500

    Update to .NET 9 from .NET 6
    Also ripped out some serialization-based exception constructors that were giving warnings for being obsolete and are never actually used anyway.

--- CvsntGitImporter/ImportFailedException.cs
+++ CvsntGitImporter/ImportFailedException.cs
@@ -1,10 +1,9 @@
-﻿/*
+/*
  * John Hall <john.hall@camtechconsultants.com>
- * Copyright (c) Cambridge Technology Consultants Ltd. All rights reserved.
+ * © 2013-2025 Cambridge Technology Consultants Ltd.
  */
 
 using System;
-using System.Runtime.Serialization;
 
 namespace CTC.CvsntGitImporter
 {
@@ -39,16 +38,6 @@ namespace CTC.CvsntGitImporter
 		/// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
 		public ImportFailedException(string message, Exception inner)
 			: base(message, inner)
-		{
-		}
-
-		/// <summary>
-		/// Initializes a new instance of the <cref>CvsGitConverter.ImportFailedException</cref> class with serialized data.
-		/// </summary>
-		/// <param name="info">The SerializationInfo that holds the serialized object data about the exception being thrown.</param>
-		/// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
-		protected ImportFailedException(SerializationInfo info, StreamingContext context)
-			: base(info, context)
 		{
 		}
 	}
